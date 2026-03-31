82ec9a616c8dcc278c7ec9e9d9b9101df621d7f8
Bruno <brunob1198@gmail.com>
2026-03-31 00:08:26 -0300

    salvar alterações

--- src/components/analise/AnaliseCustos.tsx
+++ src/components/analise/AnaliseCustos.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { format } from "date-fns";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
